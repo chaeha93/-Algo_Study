9205e5390657a66d175ede8e2a1dd5b62713c81e
chaeha93 <57698563+chaeha93@users.noreply.github.com>
2021-07-05 14:05:51 +0900

    Update and rename Main_11726_2xn타일링.java to Main_백준_11726_2xn타일링.java

--- Main_백준_11726_2xn타일링.java
+++ Main_백준_11726_2xn타일링.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main_11726_2xn타일링 {
+public class Main_백준_11726_2xn타일링 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
